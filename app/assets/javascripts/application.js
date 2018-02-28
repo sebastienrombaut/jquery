e9b1683158470cef0fa4f45f4230beaee5e1bc3d
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-02-28 03:06:07 +0100

    commentaires js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,10 @@
 //= require jquery
 //= require jquery_ujs
 
+//fonction qui englobe tout le js 
 $(document).ready(function(){
 
-//alert("JS fonctionne");
-
-
+//definitions de variables sélecteurs jquery
 $presentation = $("#presentation");
 $menu = $("#menu");
 $bieng = $("#bieng");
@@ -31,39 +30,46 @@ $bieng_list = $("#bieng_list");
 $left = $("#left");
 $right = $("#right");
 
+//Je masque mes listes au chargement 
 $presentation_list.hide();
 $menu_list.hide();
 $bieng_list.hide();
 
+//Lorsque je clique sur présentation, je fais apparaitre le bon menu
 $presentation.on('click', function() {
 	$menu_list.slideUp();
     $bieng_list.slideUp();
 	$presentation_list.slideDown();
 });
 
+//idem menu
 $menu.on('click', function() {
 	$presentation_list.slideUp();
     $bieng_list.slideUp();
 	$menu_list.slideDown();
 });
 
+//idem bieng
 $bieng.on('click', function() {
 	$menu_list.slideUp();
     $presentation_list.slideUp();
 	$bieng_list.slideDown();
 });
 
+//Je masque toutes les div supérieures à l'élement 0 dans le carousel
 $("#carousel > div:gt(0)").hide();
 
+//Toutes les 5 sec, j'effectue la série d'action suivante sur la première div du carousel
 setInterval(function() {
   $('#carousel > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#carousel');
+    .fadeOut(1000) //je la fais disparaitre
+    .next() //je passe à la suivante
+    .fadeIn(1000) // je la fais apparaitre
+    .end() //reviens sur la première div du carousel
+    .appendTo('#carousel'); //déplace cette première div à la fin du carousel pour avoir une autre div en première et faire défiler le carousel
 }, 5000);
 
+//fonction click droit, idem qu'au dessus
 $right.on('click', function() {
     $('#carousel > div:first')
     .fadeOut(1000)
@@ -73,12 +79,13 @@ $right.on('click', function() {
     .appendTo('#carousel');
 });
 
+//fonction click gauche
 $left.on('click', function() {
-    $('#carousel > div:first')
-    .fadeOut(1000)
-    $('#carousel > div:last')
-    .fadeIn(1000)
-    .prependTo('#carousel');
+    $('#carousel > div:first') 
+    .fadeOut(1000) //je fais disparaitre la div à l'écran
+    $('#carousel > div:last') // je sélectionne la dernière div du carousel
+    .fadeIn(1000) // je la fais apparaitre
+    .prependTo('#carousel'); // je l'ajoute en première position dans le carousel pour que ca redéfile à partir de celle ci
 });
 
 
